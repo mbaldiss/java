50f0f0d705761c37269fc600da16d9ae2ab92eef
Marco Baldissone <mbaldissone@pickit.net>
2024-11-25 09:34:11 -0300

    First commit. Hola Mundo!.

---
+++ src/HolaMundo.java
@@ -0,0 +1,8 @@
+public class HolaMundo {
+    // Funcion principal main
+    public static void main(String[] args){
+        System.out.println("Hola Mundo!.");
+        int age = 40;
+        System.out.print(age);
+    }
+}
