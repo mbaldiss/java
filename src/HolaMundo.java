ec4f38d3b98f05e5869f1f20b7606821309171c1
Marco Baldissone <marcol2c4@gmail.com>
2024-12-08 16:11:10 -0300

    Change age from 40 to 41.

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -1,8 +1,6 @@
 public class HolaMundo {
     // Funcion principal main
     public static void main(String[] args){
-        System.out.println("Hola Mundo!.");
-        int age = 41;
-        System.out.print(age);
+        System.out.println("Hola Mundo!");
     }
 }
---
+++ src/Variables.java
@@ -0,0 +1,8 @@
+public class Variables {
+    public static void main(String[] args){
+        int age = 40;
+        double height = 1.78;
+        String name = "Marco";
+        System.out.println("My name is " + name + ", my height is " + height + " and my age is " + age + " years old.");
+    }
+}
